98999476d9d2bd36dde1412c36b7dbbc480e7cf5
fecton <bopongamer@gmail.com>
2024-04-11 18:49:51 +0200

    Init

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+  session_start();
+  require_once 'header.php';
+
+  echo "<div class='center'>Welcome to Andrii's Campfire,";
+
+  if ($loggedin) echo " $user, you are logged in";
+  else           echo ' please sign up or log in';
+
+  echo <<<_END
+      </div><br>
+    </div>
+    <div data-role="footer">
+      <h4>Andrii is on <i><a href='https://github.com/fecton'
+      target='_blank'>GitHub</a></i></h4>
+    </div>
+  </body>
+</html>
+_END;
+
