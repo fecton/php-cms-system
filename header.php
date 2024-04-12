8610492221d56e1ec95c6a507e072a2c02290271
fecton <bopongamer@gmail.com>
2024-04-12 20:01:52 +0200

    Move js files to 'js' folder

--- header.php
+++ header.php
@@ -9,9 +9,9 @@ echo <<<_INIT
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' href='jquery.mobile-1.4.5.min.css'>
     <link rel='stylesheet' href='styles.css' type='text/css'>
-    <script src='javascript.js'></script>
-    <script src='jquery-2.2.4.min.js'></script>
-    <script src='jquery.mobile-1.4.5.min.js'></script>
+    <script src='js/javascript.js'></script>
+    <script src='js/jquery-2.2.4.min.js'></script>
+    <script src='js/jquery.mobile-1.4.5.min.js'></script>
 
 _INIT;
 
